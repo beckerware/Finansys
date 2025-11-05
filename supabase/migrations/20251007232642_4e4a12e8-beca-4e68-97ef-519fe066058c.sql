51578387cf101528c4217cef1d1480d00c27dff8
Patrick Oliveira Vagetti <77699722+DevFalconsz@users.noreply.github.com>
2025-11-05 19:29:53 -0300

    Delete supabase directory

--- supabase/migrations/20251007232642_4e4a12e8-beca-4e68-97ef-519fe066058c.sql
+++
@@ -1,4 +0,0 @@
--- Add new roles to app_role enum
-ALTER TYPE app_role ADD VALUE IF NOT EXISTS 'analista';
-ALTER TYPE app_role ADD VALUE IF NOT EXISTS 'caixa';
-ALTER TYPE app_role ADD VALUE IF NOT EXISTS 'contador';
